f2f3dc7db6d3aae9ae1076b9d1d3bb30e9ab708c
hubwriter <hubwriter@github.com>
2021-11-25 15:37:19 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,1 @@
-echo WORKING DIR
-pwd
-
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-echo SCRIPT PATH
-echo $SCRIPTPATH
-
-sudo unlink ~/.bash_profile
-ln -sv $SCRIPTPATH/.bash_profile ~
-
-source ~/.bash_profile
+source ./.bash_profile
