3b3175495396f37948942e0308d4a32977c9a96c
hubwriter <hubwriter@github.com>
2022-10-18 15:12:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,18 +6,12 @@ if ("$CODESPACES" == "true") then
     # Copy the vscode settings into the codespace:
     cp vscode/settings.json ~/.vscode-remote/data/Machine/settings.json
 
-    # Fix the git_ps1 prompt:
-    curl -L https://raw.github.com/git/git/master/contrib/completion/git-prompt.sh > ~/.bash_git
-    source ~/.bash_git
+    # Copy files into the codespace:    
+    cp -R .scripts ~/.scripts            # .scripts directory contains the branchlister scripts
+    cp .git-prompt.sh ~/.git-prompt.sh.  # this add the Git prompt
+    cp .bashrc ~/.bashrc                 # copy the .bashrc file
+    cp .bash_profile ~/.bash_profile     # copy the .bash_profile file 
+    source ~/.bashrc                     # run the .bashrc
     
-    # Copy over the .bashrc and script files:    
-    cp -R .scripts ~/.scripts
-    cp .bashrc ~/.bashrc
-    cp .bash_profile ~/.bash_profile
-    source ~/.bashrc
-    
-    # Install useful extensions:
-    code --install-extension yoshinorin.current-file-path
-    code --install-extension sleistner.vscode-fileutils
-    code --install-extension peterbe.ghdocs-goer
+    echo "To install useful extensions, run the command: run.sh"
 fi
