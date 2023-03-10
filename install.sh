50593536b54f46a09ab9c8a8d1985bdf77ec4a91
hubwriter <hubwriter@github.com>
2023-03-10 10:43:27 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ if ("$CODESPACES" == "true") then
 
    sudo apt-add-repository ppa:fish-shell/release-3 
    sudo apt-get update && sudo apt-get upgrade
-   sudo apt-get install fish
-   sudo chsh -s /usr/local/bin/fish
+   sudo apt-get install --yes fish
+   sudo chsh "$(id -un)" --shell "/usr/local/bin/fish"
 
 fi
