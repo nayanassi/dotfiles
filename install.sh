17099aa19a28be2f4d393642b365d3fc30d33a7c
hubwriter <hubwriter@github.com>
2023-03-10 10:41:45 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,17 +4,22 @@
 
 if ("$CODESPACES" == "true") then
     # Copy the vscode settings into the codespace:
-    cp vscode/settings.json ~/.vscode-remote/data/Machine/settings.json
+    #   cp vscode/settings.json ~/.vscode-remote/data/Machine/settings.json
 
     # Copy files into the codespace:    
-    cp -R .scripts ~/.scripts            # .scripts directory contains the branchlister scripts
-    cp run.sh ~/run.sh                   # add the script for installing extensions
-    cp .git-prompt.sh ~/.git-prompt.sh   # this add the Git prompt
-    cp .bashrc ~/.bashrc                 # copy the .bashrc file
-    cp .bash_profile ~/.bash_profile     # copy the .bash_profile file 
-    source ~/.bashrc                     # run the .bashrc
+    #   cp -R .scripts ~/.scripts            # .scripts directory contains the branchlister scripts
+    #   cp run.sh ~/run.sh                   # add the script for installing extensions
+    #   cp .git-prompt.sh ~/.git-prompt.sh   # this add the Git prompt
+    #   cp .bashrc ~/.bashrc                 # copy the .bashrc file
+    #   cp .bash_profile ~/.bash_profile     # copy the .bash_profile file 
+    #   source ~/.bashrc                     # run the .bashrc
     
-    sudo apt-get install --yes zsh
-    sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+    #   sudo apt-get install --yes zsh
+    #   sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+
+   sudo apt-add-repository ppa:fish-shell/release-3 
+   sudo apt-get update && sudo apt-get upgrade
+   sudo apt-get install fish
+   sudo chsh -s /usr/local/bin/fish
 
 fi
