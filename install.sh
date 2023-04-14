648d1e82c156fde02eb35ffcfc1e0c487329fc06
hubwriter <hubwriter@github.com>
2023-04-14 11:58:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,23 +3,20 @@
 # If we are in a codespace:
 
 if ("$CODESPACES" == "true") then
-    # Copy the vscode settings into the codespace:
-    cp vscode/settings.json ~/.vscode-remote/data/Machine/settings.json
+   # Copy the vscode settings into the codespace:
+   cp vscode/settings.json ~/.vscode-remote/data/Machine/settings.json
 
-    # Copy files into the codespace:    
-  #  cp -R .scripts ~/.scripts            # .scripts directory contains the branchlister scripts
-  #  cp run.sh ~/run.sh                   # add the script for installing extensions
-  #  cp .git-prompt.sh ~/.git-prompt.sh   # this add the Git prompt
-  #  cp .bashrc ~/.bashrc                 # copy the .bashrc file
-  #  cp .bash_profile ~/.bash_profile     # copy the .bash_profile file 
-  #  source ~/.bashrc                     # run the .bashrc
+   # Copy files into the codespace:    
+   cp -R .scripts ~/.scripts            # .scripts directory contains the branchlister scripts
+   cp run.sh ~/run.sh                   # add the script for installing extensions
+   cp .git-prompt.sh ~/.git-prompt.sh   # this add the Git prompt
+   cp .bashrc ~/.bashrc                 # copy the .bashrc file
+   cp .bash_profile ~/.bash_profile     # copy the .bash_profile file 
+   source ~/.bashrc                     # run the .bashrc
     
-    # Change the shell to zsh for CLI connections over SSH:
-    # sudo apt-get install --yes zsh
-    # sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
-
-sudo apt-get update -y
-sudo apt-get install -y csh
-sudo chsh "$(id -un)" --shell "/usr/bin/csh"
-
+   # Change the default shell to zsh for CLI connections over SSH:
+   # sudo apt-get update -y
+   # sudo apt-get install -y zsh
+   # sudo chsh "$(id -un)" --shell "/usr/bin/zsh"
+    
 fi
